ec48068b6672ad92531f69ac6d2f32daf360a638
Maj Bregar <maj.bregar@gmail.com>
2023-02-25 16:03:11 +0100

    dodana funkcija ZA izracun grav pospeska (in tudi moja slikica ;) )

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,19 @@
 public class Gravitacija
 {
+	private static final double M = 5.972E24;
+	private static final double R = 6.371E6;
+	private static final double G = 6.674E-11; 
+
+
 	public static void main(String[] args)
 	{
+		
 		System.out.println("OIS je zakon!");
 	}
+
+	private static double pospesek(double visina){
+		return G*M/((R+visina)*(R+visina));
+	}
+
+
 }
